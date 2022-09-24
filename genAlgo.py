c43e1f759c6dc949b7c0f3c57237c22e0c60aa5b
Gordon Moore <gjmoore13@gmail.com>
2022-09-23 22:39:01 -0600

    renderer now uses list of polygons

--- genAlgo.py
+++ genAlgo.py
@@ -1,19 +1,41 @@
 import screenSizer
 import random
+import numpy as np
+from PIL import Image
 
-vertex_list = []
+poly_List = []
+color_list = []
 filepath = "waveResize.jpg"
 
 for i in range(50):
-    vertex_list.append(((random.randint(0, screenSizer.getImgSize(filepath)[0]), random.randint(0, screenSizer.getImgSize(filepath)[1])),
+    poly_List.append(((random.randint(0, screenSizer.getImgSize(filepath)[0]), random.randint(0, screenSizer.getImgSize(filepath)[1])),
                         (random.randint(0, screenSizer.getImgSize(filepath)[0]), random.randint(0, screenSizer.getImgSize(filepath)[1])),
                         (random.randint(0, screenSizer.getImgSize(filepath)[0]), random.randint(0, screenSizer.getImgSize(filepath)[1])),
-                        (random.randint(0, screenSizer.getImgSize(filepath)[0]), random.randint(0, screenSizer.getImgSize(filepath)[1]))
-                        ))      
+                        (random.randint(0, screenSizer.getImgSize(filepath)[0]), random.randint(0, screenSizer.getImgSize(filepath)[1])),
+                        #color tuple
+                        (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                        ))  
 
-def grabRandomTriangle():
-    thing = vertex_list[random.randint(0,50)]
-    point_list = [thing[0][0], thing[0][1], thing[1][0], thing[1][1], thing[2][0], thing[2][1], thing[3][0], thing[3][1]]
-    return point_list
+def getPoly(poly):
+    points = [[poly[0][0], poly[0][1], poly[1][0], poly[1][1], poly[2][0], poly[2][1], poly[3][0], poly[3][1]],(poly[4][0], poly[4][1], poly[4][2])]
+    return points
 
+def getPolyList():
+    return poly_List
 
+def genRandomPoly():
+    return poly_List[random.randint(0,49)]
+
+def grabRandomPoly():
+    thing = genRandomPoly()
+    points = [[thing[0][0], thing[0][1], thing[1][0], thing[1][1], thing[2][0], thing[2][1], thing[3][0], thing[3][1]],(thing[4][0], thing[4][1], thing[4][2])]
+    return points
+
+def genColor():
+    poly = genRandomPoly()
+    return ((poly[4][0], poly[4][1], poly[4][2]))
+
+optimal = np.asarray(Image.open(filepath), dtype = int)
+def fitness():
+    generated_IMG = np.asarray(Image.open("screenshot.png"), dtype = int)
+    return np.sqrt(((optimal - generated_IMG)**2).sum(axis=-1)).sum()
--- renderer.py
+++ renderer.py
@@ -11,40 +11,35 @@ def getImgSize(filepath):
     img = Image.open(filepath)
     return img.size
 
-window = pyglet.window.Window(getImgSize(filepath)[0], getImgSize(filepath)[1], "Original Image")
+#window = pyglet.window.Window(getImgSize(filepath)[0], getImgSize(filepath)[1], "Original Image")
 image = pyglet.resource.image(filepath)
 
 window2 = pyglet.window.Window(getImgSize(filepath)[0], getImgSize(filepath)[1], "Geneticly Cloned Image")
 main_Batch = pyglet.graphics.Batch()
 
-point_list = [59, 149, 328, 204, 305, 284, 3, 197] #, 25, 107, 18, 61
-ec = int(len(point_list)/2)
-main_Batch.add(ec, pyglet.gl.GL_TRIANGLE_FAN, None, 
-    ("v2i", genAlgo.grabRandomTriangle()), #genAlgo.grabRandomTriangle()
-    ("c3B", (255, 0, 0) * ec))
+#ec = int(len(point_list)/2)
 
-
-
-@window.event
-def on_draw():
-    window.clear()
-    image.blit(0, 0)
+for poly in genAlgo.getPolyList():
+    poly = genAlgo.getPoly(poly)
+    ec = int(len(poly[0])/2)
+    main_Batch.add(ec, pyglet.gl.GL_TRIANGLE_FAN, None, 
+                    ("v2i", poly[0]), #genAlgo.grabRandomPoly()
+                    ("c3B", poly[1] * ec)) #genAlgo.genColor()
 
 @window2.event
 def on_draw():
     window2.clear()
     main_Batch.draw()
-
+    pyglet.image.get_buffer_manager().get_color_buffer().save('screenshot.png')
 
 # key press event    
-@window.event
+@window2.event
 def on_key_press(symbol, modifier):
     # key "C" get press
     if symbol == pyglet.window.key.ESCAPE:
         # close the window
-        window.close()
+        window2.close()
         # window2.close()
-
 
 #window.set_location(100, 400)
 #window2.set_location(1300, 400)
